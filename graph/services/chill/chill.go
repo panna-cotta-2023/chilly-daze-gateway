9ee91092a1afcbc375f389e4694f5c8ba7923d12
Takeru Otsu <takeru.o.2002@gmail.com>
2023-12-11 18:38:11 +0900

    Add chenge string timestamp to time.Time timestamp

--- graph/services/chill/chill.go
+++ graph/services/chill/chill.go
@@ -4,6 +4,8 @@ import (
 	"chilly_daze_gateway/graph/model"
 	"context"
 	"database/sql"
+	"log"
+	"time"
 )
 
 type ChillService struct {
@@ -14,5 +16,13 @@ func (u *ChillService) StartChill(
 	ctx context.Context,
 	input model.StartChillInput,
 ) (*model.Chill, error) {
-	
+	timestamp := time.Now()
+	if input.Timestamp != nil {
+		timestamp, err := time.Parse(time.RFC3339, *input.Timestamp)
+		if err != nil {
+			log.Println("time.Parse error:", err)
+			return nil, err
+		}
+	}
+
 }
