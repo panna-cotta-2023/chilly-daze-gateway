4e95338339d51a31550a151992e801071f0c7e6e
Takeru Otsu <takeru.o.2002@gmail.com>
2023-12-12 12:30:41 +0900

    Update chill service

---
+++ graph/services/chill/chill.go
@@ -0,0 +1,38 @@
+package chill
+
+import (
+	"chilly_daze_gateway/graph/model"
+	"chilly_daze_gateway/graph/db"
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+type ChillService struct {
+	Exec boil.ContextExecutor
+}
+
+func (u *ChillService) AddChill(
+	ctx context.Context,
+	startChill model.StartChillInput,
+	tracePoints []*model.TracePoint,
+	photos []*model.Photo,
+) (*model.Chill, error) {
+	result := &model.Chill{
+		ID: uuid.New().String(),
+		Traces: tracePoints,
+		Photos: photos,
+	}
+
+	db_chill := &db.Chill{
+		ID: result.ID,
+	}
+
+	err := db_chill.Insert(ctx, u.Exec, boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
