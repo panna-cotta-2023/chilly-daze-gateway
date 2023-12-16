34c9fd71230d038e4a422b452a899a71be1a5cf5
Takeru Otsu <takeru.o.2002@gmail.com>
2023-12-16 22:33:07 +0900

    Update remove warning

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -132,13 +132,3 @@ type chillResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type userResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) AddTracePoint(ctx context.Context, input []*model.TracePointInput) ([]*model.TracePoint, error) {
-	panic(fmt.Errorf("not implemented: AddTracePoint - AddTracePoint"))
-}
